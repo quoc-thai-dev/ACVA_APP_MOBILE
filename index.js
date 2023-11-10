3a668e19a6ba1c6a5ca6407580ce94a80cb0988c
Nguyễn Dương Quốc Thái <quocthai0099@gmail.com>
2023-11-10 13:04:00 +0700

    update onesignal

--- index.js
+++ index.js
@@ -5,16 +5,16 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
-import { LogLevel, OneSignal } from 'react-native-onesignal';
 // OneSignal Initialization
 // OneSignal.setAppId("76a1b2da-bc39-496b-914a-1f97dc52d7af");
 // OneSignal.setAppId('43e8a7f1-aaa7-4fef-8eaa-8f39011fef01');
+import { LogLevel, OneSignal } from 'react-native-onesignal';
 
 // Remove this method to stop OneSignal Debugging
 OneSignal.Debug.setLogLevel(LogLevel.Verbose);
 
 // OneSignal Initialization
-OneSignal.initialize("ONESIGNAL_APP_ID");
+OneSignal.initialize("76a1b2da-bc39-496b-914a-1f97dc52d7af");
 
 // requestPermission will show the native iOS or Android notification permission prompt.
 // We recommend removing the following code and instead using an In-App Message to prompt for notification permission
